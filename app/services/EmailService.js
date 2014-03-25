d9319f5486c3381434783921a12cd050e7241a20
Tim Kendrick <timkendrick@gmail.com>
2014-03-25 09:37:06 +0000

    Added stub for email service

---
+++ app/services/EmailService.js
@@ -0,0 +1,14 @@
+module.exports = (function() {
+	'use strict';
+
+	function EmailService() {
+
+	}
+
+	EmailService.prototype.send = function(to, from, subject, body, callback) {
+		// TODO: Implement email service
+	};
+
+	return EmailService;
+
+})();
--- config.js
+++ config.js
@@ -8,4 +8,7 @@ config.dropbox.appToken = process.env.DROPBOX_APP_TOKEN || null;
 config.mongodb = {};
 config.mongodb.uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || process.env.MONGODB_URI || null;
 
+config.email = {};
+config.email.from = { 'name': 'Dropkick Admin', 'address': 'admin@dropkick.it' };
+
 module.exports = config;
