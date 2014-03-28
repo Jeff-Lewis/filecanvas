f4dda7d5f76968775cf6391a651483b98b9cae3d
Tim Kendrick <timkendrick@gmail.com>
2014-03-28 15:09:16 +0000

    Throw errors if config information is invalid

--- server.js
+++ server.js
@@ -17,6 +17,12 @@
 	var debugMode = (process.env.DEBUG === 'true');
 
 
+	if (!config.dropbox.appKey) { throw new Error('Missing Dropboox app key'); }
+	if (!config.dropbox.appSecret) { throw new Error('Missing Dropboox app secret'); }
+	if (!config.mongodb.uri) { throw new Error('Missing MongoDB connection URI'); }
+	if (!config.email.from) { throw new Error('Missing email sender details'); }
+	if (!config.templates['default']) { throw new Error('Missing default template name'); }
+
 	if (!config.dropbox.appToken) {
 		console.warn('No Dropbox app token specified. Generating app token...');
 		generateDropboxToken(config, _handleDropboxTokenGenerated);
