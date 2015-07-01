6d1a5e05550d24454d1a58e2a527490f9e7953fa
Tim Kendrick <timkendrick@gmail.com>
2015-07-01 13:57:56 +0100

    Sanitize MongoDB init script

--- db/init.js
+++ db/init.js
@@ -1,37 +1,32 @@
 print('');
-print('Initialising database:');
+print('Initializing database:');
 print('');
 
 
 print(' - Switching to database \'shunt\'...');
-db = db.getSiblingDB('shunt');
+var shunt = db.getSiblingDB('shunt');
 
 
 print(' - Dropping \'users\' collection...');
-db.users.drop();
+shunt.users.drop();
 
 print(' - Dropping \'sites\' collection...');
-db.sites.drop();
+shunt.sites.drop();
 
 
 print(' - Setting \'uid\' index on \'users\' collection...');
-db.users.ensureIndex({ 'uid': 1 }, { 'unique': true });
+shunt.users.ensureIndex({ 'uid': 1 }, { 'unique': true });
 print(' - Setting \'alias\' index on \'users\' collection...');
-db.users.ensureIndex({ 'alias': 1 }, { 'unique': true });
+shunt.users.ensureIndex({ 'alias': 1 }, { 'unique': true });
 
 print(' - Setting \'user, alias\' index on \'sites\' collection...');
-db.sites.ensureIndex({ 'user': 1, 'alias': 1 }, { 'unique': true });
+shunt.sites.ensureIndex({ 'user': 1, 'alias': 1 }, { 'unique': true });
 print(' - Setting \'user\' index on \'sites\' collection...');
-db.sites.ensureIndex({ 'user': 1 });
-
-print(' - Setting \'name\' index on \'domains\' collection...');
-db.domains.ensureIndex({ 'name': 1 });
-print(' - Setting \'user, site\' index on \'domains\' collection...');
-db.domains.ensureIndex({ 'user': 1, 'site': 1 }, { 'unique': true });
+shunt.sites.ensureIndex({ 'user': 1 });
 
 
-print(' - Adding \'shunt\' user to \'users\' collection...');
-db.users.save({
+print(' - Adding \'timkendrick\' user to \'users\' collection...');
+shunt.users.save({
 	'uid': 251378090,
 	'token': null,
 	'alias': 'timkendrick',
@@ -42,7 +37,7 @@ db.users.save({
 
 
 print(' - Adding \'shunt/extranet\' site to \'sites\' collection...');
-db.sites.save({
+shunt.sites.save({
 	'user': 251378090,
 	'alias': 'acme',
 	'name': 'Acme Extranet',
@@ -57,11 +52,9 @@ db.sites.save({
 			'salt': '14io6xWHl:GGp}x-y@gvuMNg|k:7BO&M1*7M7%}p'
 		}
 	],
-	'domains': [
-		'acme-extranet.example.com'
-	],
 	'cache': null
 });
+
 
 print('');
 print('Database initialized successfully.');
