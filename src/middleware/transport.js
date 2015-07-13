9c66233796348a80fee09c45ee965eb1b8343ddd
Tim Kendrick <timkendrick@gmail.com>
2015-07-13 14:32:14 +0100

    Reorganize transport middleware

--- src/middleware/transport.js
+++ src/middleware/transport.js
@@ -6,21 +6,36 @@ var methodOverride = require('method-override');
 
 module.exports = function() {
 	var app = express();
-
-	app.use(express.cookieParser());
-	app.use(express.session({ secret: generateRandomBase64String(128) }));
-	app.use(express.json());
-	app.use(express.urlencoded());
-	app.use(methodOverride(function(req, res) {
-		if (req.body && req.body._method) {
-			var method = req.body._method;
-			delete req.body._method;
-			return method;
-		}
-	}));
-	app.use(methodOverride('X-HTTP-Method-Override'));
-
+	initSessions(app);
+	initForms(app);
 	return app;
+
+
+	function initSessions(app) {
+		app.use(express.cookieParser());
+		app.use(express.session({ secret: generateRandomBase64String(128) }));
+	}
+
+	function initForms(app) {
+		initBodyParser(app);
+		initMethodOverride(app);
+
+		function initBodyParser(app) {
+			app.use(express.json());
+			app.use(express.urlencoded());
+		}
+
+		function initMethodOverride(app) {
+			app.use(methodOverride(function(req, res) {
+				if (req.body && req.body._method) {
+					var method = req.body._method;
+					delete req.body._method;
+					return method;
+				}
+			}));
+			app.use(methodOverride('X-HTTP-Method-Override'));
+		}
+	}
 
 
 	function generateRandomBase64String(numBytes) {
