24d1bdcea255a40c1d97017c865b21f089445b76
Tim Kendrick <timkendrick@gmail.com>
2015-07-14 23:14:17 +0100

    Throw HTTP 400 error if accessed via invalid domain

--- src/middleware/customDomain.js
+++ src/middleware/customDomain.js
@@ -2,6 +2,7 @@
 
 var dns = require('dns');
 
+var HttpError = require('../errors/HttpError');
 var getIsSameHost = require('../utils/getIsSameHost');
 
 module.exports = function(options) {
@@ -14,9 +15,13 @@ module.exports = function(options) {
 			return next();
 		}
 		dns.resolveCname(req.host, function(error, cnames) {
+			if (error && (error.code === 'ENODATA' || error.code === 'ENOTFOUND')) {
+				error = null;
+				cnames = null;
+			}
 			if (error) { return next(error); }
 			if (!cnames || (cnames.length === 0)) {
-				return next();
+				return next(new HttpError(400, 'Invalid host: ' + req.host));
 			}
 			var cname = cnames[0];
 			res.locals.originalHost = req.host;
