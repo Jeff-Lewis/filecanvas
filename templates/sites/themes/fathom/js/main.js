bd579fc489e15ab68ea9c057340323fa8b4f9a0d
Tim Kendrick <timkendrick@gmail.com>
2015-08-22 16:26:37 +0100

    Add lightbox to Fathom site template

--- templates/sites/themes/fathom/js/main.js
+++ templates/sites/themes/fathom/js/main.js
@@ -1,3 +1,44 @@
 $(document).ready(function(){
+	initFilePreview();
 
+
+	function initFilePreview() {
+		var IMAGE_PREVIEW_EXTENSIONS = [
+			'jpg',
+			'jpeg',
+			'gif',
+			'png'
+		];
+		var DOCUMENT_PREVIEW_EXTENSIONS = [
+			'html',
+			'pdf',
+			'txt'
+		];
+		$('.post a.project-wrp').click(function(event) {
+			var $element = $(this);
+			var downloadUrl = $element.attr('href');
+			var extension = downloadUrl.split('.').pop();
+			var canShowImagePreview = IMAGE_PREVIEW_EXTENSIONS.indexOf(extension) !== -1;
+			if (canShowImagePreview) {
+				event.preventDefault();
+				$.fancybox.open(downloadUrl, {
+					type: 'image',
+					afterLoad: onLightboxOpened
+				});
+			}
+			var canShowDocumentPreview = DOCUMENT_PREVIEW_EXTENSIONS.indexOf(extension) !== -1;
+			if (canShowDocumentPreview) {
+				event.preventDefault();
+				$.fancybox.open(downloadUrl, {
+					type: 'iframe',
+					afterLoad: onLightboxOpened
+				});
+			}
+
+
+			function onLightboxOpened() {
+				$('.fancybox-inner').wrap('<a class="fancybox-download" href="' + downloadUrl + '" download onclick="$.fancybox.close()"></a>');
+			}
+		});
+	}
 });
