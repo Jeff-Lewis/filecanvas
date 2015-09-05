8f5ac1a69ab65929f813756a596be753916520b8
Tim Kendrick <timkendrick@gmail.com>
2015-09-05 18:36:59 +0100

    Refactor Handlebars template engine

--- src/engines/handlebars.js
+++ src/engines/handlebars.js
@@ -5,81 +5,116 @@ var Handlebars = require('handlebars');
 
 var templateCache = {};
 
-Handlebars.registerHelper('eq', function(item1, item2, options) {
-	return item1 === item2;
-});
-Handlebars.registerHelper('not-eq', function(item1, item2, options) {
-	return item1 !== item2;
-});
-Handlebars.registerHelper('not', function(item, options) {
-	return !item;
-});
-Handlebars.registerHelper('and', function(item1, item2, options) {
-	return item1 && item2;
-});
-Handlebars.registerHelper('or', function(item1, item2, options) {
-	return item1 || item2;
-});
-Handlebars.registerHelper('gt', function(item1, item2, options) {
-	return item1 > item2;
-});
-Handlebars.registerHelper('gte', function(item1, item2, options) {
-	return item1 >= item2;
-});
-Handlebars.registerHelper('lt', function(item1, item2, options) {
-	return item1 >= item2;
-});
-Handlebars.registerHelper('lte', function(item1, item2, options) {
-	return item1 >= item2;
-});
-Handlebars.registerHelper('is-array', function(item, options) {
-	return Array.isArray(item);
-});
-
-Handlebars.registerHelper('replace', function(item1, item2, options) {
-	return options.fn(this).replace(item1, item2);
-});
-Handlebars.registerHelper('concat', function(item, options) {
-	var items = Array.prototype.slice.call(arguments, 0, -1);
-	return items.join('');
-});
-Handlebars.registerHelper('startsWith', function(haystack, needle, options) {
-	return haystack.indexOf(needle) === 0;
-});
-
 module.exports = function(filePath, options, callback) {
-	loadTemplate(filePath, function(error, template) {
-		if (error) { return callback(error); }
-		var context = options;
-		var templateOptions = options._ || {};
-		var output = template(context, templateOptions);
-		callback(null, output);
-	});
+	var context = options;
+	loadTemplate(filePath, context)
+		.then(function(templateFunction) {
+			var templateOptions = options._ || {};
+			var output = templateFunction(context, templateOptions);
+			callback(null, output);
+		})
+		.catch(function(error) {
+			return callback(error);
+		});
 };
 
-function loadTemplate(templatePath, callback) {
-	if (templatePath in templateCache) {
-		loadCachedTemplate(templatePath, templateCache, callback);
+function loadTemplate(templatePath, context) {
+	var hasCachedTemplate = getHasCachedTemplate(templatePath, templateCache);
+	if (hasCachedTemplate) {
+		var cachedTemplate = retrieveCachedTemplate(templatePath, templateCache);
+		return Promise.resolve(cachedTemplate);
 	} else {
-		loadTemplate(templatePath, function(error, templateFunction) {
-			templateCache[templatePath] = templateFunction;
-			callback(null, templateFunction);
-		});
+		return loadTemplate(templatePath, context)
+			.then(function(templateFunction) {
+				cacheTemplate(templateFunction, templatePath, templateCache);
+				return templateFunction;
+			});
 	}
 
 
 	function loadTemplate(templatePath, callback) {
-		fs.readFile(templatePath, { encoding: 'utf-8' }, function(error, data) {
-			if (error) { return callback(error); }
-			var templateFunction = Handlebars.compile(data);
-			callback(null, templateFunction);
+		return new Promise(function(resolve, reject) {
+			var compiler = createHandlebarsCompiler();
+			fs.readFile(templatePath, { encoding: 'utf-8' }, function(error, templateSource) {
+				if (error) { return reject(error); }
+				var templateFunction = compiler.compile(templateSource);
+				resolve(templateFunction);
+			});
 		});
 	}
 
-	function loadCachedTemplate(templatePath, templateCache, callback) {
-		process.nextTick(function() {
-			var templateFunction = templateCache[templatePath];
-			callback(null, templateFunction);
-		});
+	function getHasCachedTemplate(templatePath, templateCache) {
+		return (templatePath in templateCache);
+	}
+
+	function cacheTemplate(templateFunction, templatePath, templateCache) {
+		templateCache[templatePath] = templateFunction;
+	}
+
+	function retrieveCachedTemplate(templatePath, templateCache) {
+		return templateCache[templatePath];
+	}
+}
+
+function createHandlebarsCompiler() {
+	var compiler = Handlebars.create();
+	registerHelpers(compiler);
+	return compiler;
+
+
+	function registerHelpers(compiler) {
+		registerBooleanHelpers(compiler);
+		registerArrayHelpers(compiler);
+		registerStringHelpers(compiler);
+
+
+		function registerBooleanHelpers(compiler) {
+			compiler.registerHelper('eq', function(item1, item2, options) {
+				return item1 === item2;
+			});
+			compiler.registerHelper('not-eq', function(item1, item2, options) {
+				return item1 !== item2;
+			});
+			compiler.registerHelper('not', function(item, options) {
+				return !item;
+			});
+			compiler.registerHelper('and', function(item1, item2, options) {
+				return item1 && item2;
+			});
+			compiler.registerHelper('or', function(item1, item2, options) {
+				return item1 || item2;
+			});
+			compiler.registerHelper('gt', function(item1, item2, options) {
+				return item1 > item2;
+			});
+			compiler.registerHelper('gte', function(item1, item2, options) {
+				return item1 >= item2;
+			});
+			compiler.registerHelper('lt', function(item1, item2, options) {
+				return item1 >= item2;
+			});
+			compiler.registerHelper('lte', function(item1, item2, options) {
+				return item1 >= item2;
+			});
+		}
+
+		function registerArrayHelpers(compiler) {
+			compiler.registerHelper('is-array', function(item, options) {
+				return Array.isArray(item);
+			});
+		}
+
+		function registerStringHelpers(compiler) {
+			compiler.registerHelper('replace', function(item1, item2, options) {
+				return options.fn(this).replace(item1, item2);
+			});
+			compiler.registerHelper('concat', function(item, options) {
+				var items = Array.prototype.slice.call(arguments, 0, -1);
+				return items.join('');
+			});
+			compiler.registerHelper('startsWith', function(haystack, needle, options) {
+				return haystack.indexOf(needle) === 0;
+			});
+		}
 	}
 }
