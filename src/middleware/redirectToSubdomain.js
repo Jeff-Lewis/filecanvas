cc2b7d448ed976f371136627d555e6e6722fddb3
Tim Kendrick <timkendrick@gmail.com>
2015-09-06 17:43:47 +0100

    Update `redirectToSubdomain` middleware to redirect from one subdomain to another

--- src/middleware/redirectToSubdomain.js
+++ src/middleware/redirectToSubdomain.js
@@ -5,7 +5,12 @@ module.exports = function(options) {
 	var subdomain = options.subdomain;
 
 	return function(req, res, next) {
-		var redirectUrl = '//' + subdomain + '.' + req.get('host') + req.url;
+		var host = req.get('host');
+		var currentSubdomain = req.subdomains.slice().reverse().join('.');
+		if (currentSubdomain.length > 0) {
+			host = host.slice((currentSubdomain + '.').length);
+		}
+		var redirectUrl = '//' + subdomain + '.' + host + req.url;
 		res.redirect(301, redirectUrl);
 	};
 };
