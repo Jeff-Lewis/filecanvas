2faca98733020ed2424655b3a36f1567071e8a18
Tim Kendrick <timkendrick@gmail.com>
2015-09-14 23:53:45 +0100

    Move client library compilation into install script

--- install.sh
+++ install.sh
@@ -1,2 +1,14 @@
+cd client/api
+webpack -p
+cd ../..
+
+cd client/admin
+webpack -p
+cd ../..
+
+cd client/editor
+webpack -p
+cd ../..
+
 cd node_modules/dropbox
 NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install
