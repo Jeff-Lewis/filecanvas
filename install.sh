026e5e702fd07ece1412a4277d7a58b02cd504e9
Tim Kendrick <timkendrick@gmail.com>
2015-09-17 01:44:25 +0100

    Use sub-shells to compile libraries

--- install.sh
+++ install.sh
@@ -1,14 +1,7 @@
-cd client/api
-webpack -p
-cd ../..
+# Build client libraries
+(cd client/admin; webpack -p)
+(cd client/api; webpack -p)
+(cd client/editor; webpack -p)
 
-cd client/admin
-webpack -p
-cd ../..
-
-cd client/editor
-webpack -p
-cd ../..
-
-cd node_modules/dropbox
-NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install
+# Build Dropbox library
+(cd node_modules/dropbox; NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install)
