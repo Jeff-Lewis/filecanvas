c301cec9afefb6b7065441ebfe00bf9c3d6e5cb8
Tim Kendrick <timkendrick@gmail.com>
2015-09-23 17:08:55 +0100

    Update DB init script Dropbox adapter index

--- db/init.js
+++ db/init.js
@@ -16,8 +16,8 @@ shunt.sites.drop();
 
 print(' - Setting \'username\' index on \'users\' collection...');
 shunt.users.createIndex({ 'username': 1 }, { 'unique': true });
-print(' - Setting \'providers.dropbox.uid\' index on \'users\' collection...');
-shunt.users.createIndex({ 'providers.dropbox.uid': 1 }, { 'unique': true, 'sparse': true });
+print(' - Setting \'adapters.dropbox.uid\' index on \'users\' collection...');
+shunt.users.createIndex({ 'adapters.dropbox.uid': 1 }, { 'unique': true, 'sparse': true });
 
 print(' - Setting \'owner, name\' index on \'sites\' collection...');
 shunt.sites.createIndex({ 'owner': 1, 'name': 1 }, { 'unique': true });
