dbcd4399f28b39ab4c3b1aad78f21e367616afa3
Tim Kendrick <timkendrick@gmail.com>
2015-09-25 12:38:26 +0100

    Remove database name references from DB init script

--- db/init.js
+++ db/init.js
@@ -3,26 +3,22 @@ print('Initializing database:');
 print('');
 
 
-print(' - Switching to database \'shunt\'...');
-var shunt = db.getSiblingDB('shunt');
-
-
 print(' - Dropping \'users\' collection...');
-shunt.users.drop();
+db.users.drop();
 
 print(' - Dropping \'sites\' collection...');
-shunt.sites.drop();
+db.sites.drop();
 
 
 print(' - Setting \'username\' index on \'users\' collection...');
-shunt.users.createIndex({ 'username': 1 }, { 'unique': true });
+db.users.createIndex({ 'username': 1 }, { 'unique': true });
 print(' - Setting \'adapters.dropbox.uid\' index on \'users\' collection...');
-shunt.users.createIndex({ 'adapters.dropbox.uid': 1 }, { 'unique': true, 'sparse': true });
+db.users.createIndex({ 'adapters.dropbox.uid': 1 }, { 'unique': true, 'sparse': true });
 
 print(' - Setting \'owner, name\' index on \'sites\' collection...');
-shunt.sites.createIndex({ 'owner': 1, 'name': 1 }, { 'unique': true });
+db.sites.createIndex({ 'owner': 1, 'name': 1 }, { 'unique': true });
 print(' - Setting \'owner\' index on \'sites\' collection...');
-shunt.sites.createIndex({ 'owner': 1 });
+db.sites.createIndex({ 'owner': 1 });
 
 
 print('');
