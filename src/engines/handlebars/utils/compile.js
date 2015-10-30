d17fa8a4529cb4551660474314b8c58126cd3ec0
Tim Kendrick <timkendrick@gmail.com>
2015-10-30 22:35:32 +0000

    Remove Handlebars compiler hack

--- src/engines/handlebars/utils/compile.js
+++ src/engines/handlebars/utils/compile.js
@@ -6,29 +6,13 @@ module.exports = function(templateSource, options) {
 	options = options || {};
 	var helpers = options.helpers || {};
 	var compiler = Handlebars.create();
-	var rootContext = null;
 	Object.keys(helpers).forEach(function(helperName) {
 		var helper = helpers[helperName];
-		compiler.registerHelper(helperName, function() {
-			// HACK: allow the helpers to access the root context
-			Object.defineProperty(this, '@root', {
-				value: rootContext,
-				enumerable: false,
-				configurable: true
-			});
-
-			// Invoke the helper with the modified `this` object
-			var output = helper.apply(this, arguments);
-
-			// Reset the `this` object to its unmodified state
-			delete this['@root'];
-
-			return output;
-		});
+		compiler.registerHelper(helperName, helper);
 	});
-	var templateFunction = compiler.compile(templateSource);
-	return function(context, templateOptions) {
-		rootContext = context;
-		return templateFunction(context, templateOptions);
-	};
+	var templateFunction = compiler.compile(templateSource, {
+		knownHelpers: helpers,
+		knownHelpersOnly: true
+	});
+	return templateFunction;
 };
