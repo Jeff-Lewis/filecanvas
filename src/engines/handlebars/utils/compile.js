a702161dae2d6f06739c711b9a8285c86ca7b057
Tim Kendrick <timkendrick@gmail.com>
2015-10-30 15:31:05 +0000

    Clean up after Handlebars helper `@root` hack

--- src/engines/handlebars/utils/compile.js
+++ src/engines/handlebars/utils/compile.js
@@ -13,9 +13,17 @@ module.exports = function(templateSource, options) {
 			// HACK: allow the helpers to access the root context
 			Object.defineProperty(this, '@root', {
 				value: rootContext,
-				enumerable: false
+				enumerable: false,
+				configurable: true
 			});
-			return helper.apply(this, arguments);
+
+			// Invoke the helper with the modified `this` object
+			var output = helper.apply(this, arguments);
+
+			// Reset the `this` object to its unmodified state
+			delete this['@root'];
+
+			return output;
 		});
 	});
 	var templateFunction = compiler.compile(templateSource);
