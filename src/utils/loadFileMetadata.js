1c7ea35a4123467ee93629a69183e11e84f92c37
Tim Kendrick <timkendrick@gmail.com>
2015-11-05 19:30:55 +0000

    Allow synchronous file metadata loading

--- src/utils/loadFileMetadata.js
+++ src/utils/loadFileMetadata.js
@@ -12,15 +12,41 @@ var FileModel = require('../models/FileModel');
 module.exports = function(filePath, options) {
 	options = options || {};
 	var rootPath = options.root || null;
+	var sync = Boolean(options.sync);
 	var includeContents = Boolean(options.contents);
-	return loadFileMetadata(filePath, rootPath, includeContents);
+	if (sync) {
+		return loadFileMetadataSync(filePath, rootPath, includeContents);
+	} else {
+		return loadFileMetadata(filePath, rootPath, includeContents);
+	}
 };
+
+function loadFileMetadataSync(filePath, rootPath, includeContents) {
+	var stat = fs.statSync(filePath);
+	var relativePath = getRelativePath(filePath, rootPath);
+	var fileModel = parseStatModel(stat, relativePath);
+	if (stat.isDirectory() && includeContents) {
+		fileModel.contents = loadFolderContentsSync(filePath, rootPath);
+	}
+	return fileModel;
+
+
+	function loadFolderContentsSync(folderPath, rootPath) {
+		var filenames = fs.readdirSync(folderPath);
+		filenames = filenames.filter(junk.not);
+		return filenames.map(function(filename) {
+			var filePath = path.join(folderPath, filename);
+			var includeContents = true;
+			return loadFileMetadataSync(filePath, rootPath, includeContents);
+		});
+	}
+}
 
 function loadFileMetadata(filePath, rootPath, includeContents) {
 	return new Promise(function(resolve, reject) {
 		fs.stat(filePath, function(error, stat) {
 			if (error) { return reject(error); }
-			var relativePath = filePath.replace(rootPath, '') || '/';
+			var relativePath = getRelativePath(filePath, rootPath);
 			var fileModel = parseStatModel(stat, relativePath);
 			if (stat.isDirectory() && includeContents) {
 				loadFolderContents(filePath, rootPath)
@@ -39,26 +65,27 @@ function loadFileMetadata(filePath, rootPath, includeContents) {
 			}
 		});
 	});
-}
 
-function loadFolderContents(folderPath, rootPath) {
-	return new Promise(function(resolve, reject) {
-		fs.readdir(folderPath, function(error, filenames) {
-			if (error) { return reject(error); }
-			filenames = filenames.filter(junk.not);
-			mapSeries(filenames, function(filename) {
-				var filePath = path.join(folderPath, filename);
-				var includeContents = true;
-				return loadFileMetadata(filePath, rootPath, includeContents);
-			})
-			.then(function(files) {
-				resolve(files);
-			})
-			.catch(function(error) {
-				reject(error);
+
+	function loadFolderContents(folderPath, rootPath) {
+		return new Promise(function(resolve, reject) {
+			fs.readdir(folderPath, function(error, filenames) {
+				if (error) { return reject(error); }
+				filenames = filenames.filter(junk.not);
+				mapSeries(filenames, function(filename) {
+					var filePath = path.join(folderPath, filename);
+					var includeContents = true;
+					return loadFileMetadata(filePath, rootPath, includeContents);
+				})
+				.then(function(files) {
+					resolve(files);
+				})
+				.catch(function(error) {
+					reject(error);
+				});
 			});
 		});
-	});
+	}
 }
 
 function parseStatModel(stat, filePath) {
@@ -76,4 +103,9 @@ function parseStatModel(stat, filePath) {
 		directory: isDirectory
 	};
 	return new FileModel(fileMetadata);
+}
+
+function getRelativePath(filePath, rootPath) {
+	if (!rootPath) { return filePath; }
+	return '/' + path.relative(rootPath, filePath);
 }
