b79cdf36333874c654b4811e1bcd8b7c3f99c50d
Tim Kendrick <timkendrick@gmail.com>
2015-11-10 15:08:21 +0000

    Convert Handlebars helpers to HTMLBars helpers

--- src/engines/htmlbars/helpers/index.js
+++ src/engines/htmlbars/helpers/index.js
@@ -5,4 +5,12 @@ var objectAssign = require('object-assign');
 var emberHelpers = require('@timkendrick/ember-htmlbars-helpers');
 var handlebarsHelpers = require('../../handlebars/helpers');
 
-module.exports = objectAssign(emberHelpers, handlebarsHelpers);
+var convertedHandlebarsHelpers = Object.keys(handlebarsHelpers).reduce(function(helpers, helperName) {
+	var handlebarsHelper = handlebarsHelpers[helperName];
+	helpers[helperName] = function(params, hash, blocks) {
+		return handlebarsHelper.apply(this, params.concat(blocks));
+	};
+	return helpers;
+}, {});
+
+module.exports = objectAssign(emberHelpers, convertedHandlebarsHelpers);
