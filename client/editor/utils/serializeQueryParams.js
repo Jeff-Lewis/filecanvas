3c0468af0d374885e58ab583b09be2e32a035e6c
Tim Kendrick <timkendrick@gmail.com>
2015-11-18 18:00:36 +0000

    Fix client-side URL serialization of primitives

--- client/editor/utils/serializeQueryParams.js
+++ client/editor/utils/serializeQueryParams.js
@@ -3,6 +3,20 @@
 module.exports = function(params) {
 	return Object.keys(params).map(function(key) {
 		var value = params[key];
-		return key + '=' + encodeURIComponent(JSON.stringify(value));
+		var serializedValue = serializeValue(value);
+		return (serializedValue ? key + '=' + encodeURIComponent(serializedValue) : null);
+	}).filter(function(segment) {
+		return Boolean(segment);
 	}).join('&');
 };
+
+function serializeValue(value) {
+	switch (typeof value) {
+		case 'undefined': return null;
+		case 'boolean': return value.toString();
+		case 'string': return value;
+		case 'number': return value.toString();
+		case 'object': return (value === null ? null : JSON.stringify(value));
+		default: throw new Error('Invalid query parameter: ' + value);
+	}
+}
