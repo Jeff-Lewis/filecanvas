9bb1335ae6bbc9e2e144e39656c8cf0b9251d73a
Tim Kendrick <timkendrick@gmail.com>
2015-11-19 12:13:01 +0000

    Fix HTMLBars import paths before building client code in install script

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
+# Fix HTMLBars import paths
+ln -s cjs/htmlbars-util node_modules/htmlbars/dist/htmlbars-util
+ln -s htmlbars/dist/cjs/htmlbars-runtime node_modules/htmlbars-runtime
+
 # Build client libraries
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
@@ -5,7 +9,3 @@
 
 # Build Dropbox library
 (cd node_modules/dropbox; NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install)
-
-# Fix HTMLBars import paths
-ln -s cjs/htmlbars-util node_modules/htmlbars/dist/htmlbars-util
-ln -s htmlbars/dist/cjs/htmlbars-runtime node_modules/htmlbars-runtime
