f56a7a6da61ae366bdcf71956a4640a3e570fe0e
Tim Kendrick <timkendrick@gmail.com>
2015-11-19 12:09:31 +0000

    Fix HTMLBars import paths

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@
 
 # Build Dropbox library
 (cd node_modules/dropbox; NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install)
+
+# Fix HTMLBars import paths
+ln -s cjs/htmlbars-util node_modules/htmlbars/dist/htmlbars-util
+ln -s htmlbars/dist/cjs/htmlbars-runtime node_modules/htmlbars-runtime
