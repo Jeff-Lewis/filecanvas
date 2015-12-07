840077c7a53c1a94570707550a51b88a3c7e9ad2
Tim Kendrick <timkendrick@gmail.com>
2015-12-07 15:58:15 +0000

    Update HTMLBars dependency

--- install.sh
+++ install.sh
@@ -1,7 +1,3 @@
-# Fix HTMLBars import paths
-ln -s cjs/htmlbars-util node_modules/htmlbars/dist/htmlbars-util
-ln -s htmlbars/dist/cjs/htmlbars-runtime node_modules/htmlbars-runtime
-
 # Build client libraries
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
