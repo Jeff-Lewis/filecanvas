680485f1fb419790f974feafffef012f1353c2f2
Tim Kendrick <timkendrick@gmail.com>
2015-12-07 15:58:05 +0000

    Remove unnecessary Dropbox build step from postinstall

--- install.sh
+++ install.sh
@@ -6,6 +6,3 @@ ln -s htmlbars/dist/cjs/htmlbars-runtime node_modules/htmlbars-runtime
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
 (cd client/editor; webpack -p)
-
-# Build Dropbox library
-(cd node_modules/dropbox; NODE_ENV=development NPM_CONFIG_PRODUCTION=false npm_config_production=false npm install)
