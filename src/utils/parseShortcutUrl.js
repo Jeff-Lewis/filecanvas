78b5db400df39f27cb8438f47f16eb90b138b9b9
Tim Kendrick <timkendrick@gmail.com>
2015-12-09 22:46:15 +0000

    Add shortcut file parser utility function

---
+++ src/utils/parseShortcutUrl.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var ini = require('ini');
+var plist = require('plist');
+
+module.exports = function(shortcutFileContents, options) {
+	var shortcutType = options.type;
+	switch (shortcutType) {
+		case 'url':
+			return parseWindowsShortcutUrl(shortcutFileContents);
+		case 'webloc':
+			return parseOsXShortcutUrl(shortcutFileContents);
+		case 'desktop':
+			return parseLinuxShortcutUrl(shortcutFileContents);
+		default:
+			throw new Error('Invalid shortcut file type: ' + shortcutType);
+	}
+
+
+	function parseWindowsShortcutUrl(shortcutFileContents) {
+		var data = ini.parse(shortcutFileContents);
+		return data['InternetShortcut'].URL;
+	}
+
+	function parseOsXShortcutUrl(shortcutFileContents) {
+		var data = plist.parse(shortcutFileContents);
+		return data.URL;
+	}
+
+	function parseLinuxShortcutUrl(shortcutFileContents) {
+		var data = ini.parse(shortcutFileContents);
+		return data['Desktop Entry'].URL;
+	}
+};
