97f57fa11d41f42242d0c79c8c42f4afc822817b
Tim Kendrick <timkendrick@gmail.com>
2016-01-09 18:39:04 +0000

    Update MongoDB data path

--- services/mongodb/scripts/init.js
+++ services/mongodb/scripts/init.js
@@ -1,15 +1,3 @@
-print('');
-print('Initializing database:');
-print('');
-
-
-print(' - Dropping \'users\' collection...');
-db.users.drop();
-
-print(' - Dropping \'sites\' collection...');
-db.sites.drop();
-
-
 print(' - Setting \'username\' index on \'users\' collection...');
 db.users.createIndex({ 'username': 1 }, { 'unique': true });
 print(' - Setting \'adapters.dropbox.uid\' index on \'users\' collection...');
@@ -19,8 +7,3 @@ print(' - Setting \'owner, name\' index on \'sites\' collection...');
 db.sites.createIndex({ 'owner': 1, 'name': 1 }, { 'unique': true });
 print(' - Setting \'owner\' index on \'sites\' collection...');
 db.sites.createIndex({ 'owner': 1 });
-
-
-print('');
-print('Database initialized successfully.');
-print('');
