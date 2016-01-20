366dec5ba5ffc90465a7db1fec000d64484c0465
Tim Kendrick <timkendrick@gmail.com>
2016-01-20 19:36:03 +0000

    Add nginx static server

---
+++ services/nginx/init.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf /etc/nginx/conf.d
+mkdir -p /etc/nginx/conf.d
+cp -r /etc/nginx/template.d/* /etc/nginx/conf.d
+sed -i 's/\$HOST/'$HOST'/g' /etc/nginx/conf.d/*.conf
+
+nginx -g 'daemon off;'
