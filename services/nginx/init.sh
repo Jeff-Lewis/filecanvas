99d2e01c727f68b67b1de6047427f2c66be75fed
Tim Kendrick <timkendrick@gmail.com>
2016-01-21 22:58:42 +0000

    Ensure nginx Elastic Beanstalk log directory exists

--- services/nginx/init.sh
+++ services/nginx/init.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+echo "Copying nginx configuration"
 rm -rf /etc/nginx/conf.d
 mkdir -p /etc/nginx/conf.d
 cp -r /etc/nginx/template.d/* /etc/nginx/conf.d
 sed -i 's/\$HOST/'$HOST'/g' /etc/nginx/conf.d/*.conf
+
+echo "Creating /var/log/nginx/healthd directory"
+mkdir -p /var/log/nginx/healthd
+chmod 777 /var/log/nginx/healthd
 
 nginx -g 'daemon off;'
