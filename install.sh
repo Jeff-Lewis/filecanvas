9a8b948bff034187501afe76a628f7d5a889f92f
Tim Kendrick <timkendrick@gmail.com>
2016-01-21 01:54:31 +0000

    Build www site on npm postinstall

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
+# Build www site
+if [ $WWW_SITE_ROOT ]; then
+	echo "Updating site generator npm packages..."
+	(cd services/www; npm install)
+	echo "Generating www site..."
+	HOSTNAME=$HOST
+	PROTOCOL="${HOST_PROTOCOL:-$([ "$HTTPS" == "true" ] && echo "https:" || echo "http:")}"
+	PORT="${HOST_PORT:-$([ "$PROTOCOL" == "https:" ] && echo "$HTTPS_PORT" || echo "$PORT")}"
+	HOST=$HOST HOST_PROTOCOL=$PROTOCOL HOST_PORT=$PORT TEMPLATE_DIR=./templates/www OUTPUT_DIR=$WWW_SITE_ROOT ./services/www/build
+fi
+
 # Build client libraries
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
