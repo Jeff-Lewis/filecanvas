886df57d85a3fb9522269cb46f5ad7ed04b5d603
Tim Kendrick <timkendrick@gmail.com>
2016-01-22 14:50:34 +0000

    Remove old client libraries when rebuilding

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 # Build client libraries
 echo "Building client libraries..."
+rm -f ./templates/admin/assets/js/filecanvas-*.js
+rm -f ./templates/admin/assets/js/filecanvas-*.js.map
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
 (cd client/editor; webpack -p)
