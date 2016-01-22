8dc7157ceef72997cc8bb9a25cd4b445e78fd6c2
Tim Kendrick <timkendrick@gmail.com>
2016-01-22 02:04:02 +0000

    Fix www site port defaults

--- prestart.sh
+++ prestart.sh
@@ -2,9 +2,12 @@
 
 # Build www site
 echo "Generating www site..."
+HOST_PROTOCOL=${HOST_PROTOCOL:-$([ "$HTTPS" == "true" ] && echo "https:" || echo "http:")}
+HOST_PORT=${HOST_PORT:-$([ "$HOST_PROTOCOL" == "https:" ] && echo "$HTTPS_PORT" || echo "$PORT")}
+
 HOST=$HOST \
-HOST_PROTOCOL="${HOST_PROTOCOL:-$([ "$HTTPS" == "true" ] && echo "https:" || echo "http:")}" \
-HOST_PORT="${HOST_PORT:-$([ "$PROTOCOL" == "https:" ] && echo "$HTTPS_PORT" || echo "$PORT")}" \
+HOST_PROTOCOL=$HOST_PROTOCOL \
+HOST_PORT=$HOST_PORT \
 TEMPLATE_DIR=./www/template \
 OUTPUT_DIR=$WWW_SITE_ROOT \
 	./www/build
