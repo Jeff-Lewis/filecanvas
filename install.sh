27bffb22ede82333284d590570da2a23b2ee2d59
Tim Kendrick <timkendrick@gmail.com>
2016-01-22 15:18:12 +0000

    Move client-side build from build to pre-push script

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 # Build client libraries
 echo "Building client libraries..."
-rm -f ./templates/admin/assets/js/filecanvas-*.js
-rm -f ./templates/admin/assets/js/filecanvas-*.js.map
 (cd client/admin; webpack -p)
 (cd client/api; webpack -p)
 (cd client/editor; webpack -p)
