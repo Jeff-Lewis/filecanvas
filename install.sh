f90c71c1ceced2a7c98f83565857d5b6680a51ad
Tim Kendrick <timkendrick@gmail.com>
2016-01-26 07:15:56 +0000

    Reorder postinstall script

--- install.sh
+++ install.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-echo "Building theme previews..."
-THEMES_DIR=./themes
-THEMES_OUTPUT_DIR=${THEMES_ROOT:-./data/themes}
-rm -rf $THEMES_OUTPUT_DIR/*
-mkdir -p $THEMES_OUTPUT_DIR
-for theme in $(ls $THEMES_DIR); do
-	./workers/theme/bundle $THEMES_DIR/$theme $THEMES_OUTPUT_DIR/$theme
-done
-
 echo "Building client libraries..."
 rm -f ./templates/admin/assets/js/filecanvas-*.js
 rm -f ./templates/admin/assets/js/filecanvas-*.js.map
@@ -25,3 +16,12 @@ else
 	(cd client/editor; webpack -d --watch) &
 	(cd client/overlay; webpack -d --watch) &
 fi
+
+echo "Building theme previews..."
+THEMES_DIR=./themes
+THEMES_OUTPUT_DIR=${THEMES_ROOT:-./data/themes}
+rm -rf $THEMES_OUTPUT_DIR/*
+mkdir -p $THEMES_OUTPUT_DIR
+for theme in $(ls $THEMES_DIR); do
+	./workers/theme/bundle $THEMES_DIR/$theme $THEMES_OUTPUT_DIR/$theme
+done
