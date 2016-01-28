e58d4e652592ccac536c738b01ee84cadbde522f
Tim Kendrick <timkendrick@gmail.com>
2016-01-28 17:09:30 +0000

    Ensure compiled themes directory is deleted in install script

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if [ "$NODE_ENV" != "production" ]; then
 	echo "Building theme previews..."
 	THEMES_DIR=./themes
 	THEMES_OUTPUT_DIR=${THEMES_ROOT:-./data/themes}
-	rm -rf $THEMES_OUTPUT_DIR/*
+	rm -rf $THEMES_OUTPUT_DIR
 	mkdir -p $THEMES_OUTPUT_DIR
 	for theme in $(ls $THEMES_DIR); do
 		./workers/theme/bundle $THEMES_DIR/$theme $THEMES_OUTPUT_DIR/$theme
