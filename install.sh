2e6d37ec583256d573a05259de8c1cfcde7af140
Tim Kendrick <timkendrick@gmail.com>
2016-01-28 19:56:22 +0000

    Improve dev start script

--- install.sh
+++ install.sh
@@ -4,14 +4,14 @@
 echo "Building client libraries..."
 rm -f ./templates/admin/assets/js/filecanvas-*.js
 rm -f ./templates/admin/assets/js/filecanvas-*.js.map
-if [ $DEBUG == 'true' ]; then
-	(cd client/admin; webpack -d --watch) &
-	(cd client/api; webpack -d --watch) &
-	(cd client/editor; webpack -d --watch) &
-	(cd client/overlay; webpack -d --watch) &
-else
+if [ $NODE_ENV == 'production' ]; then
 	(cd client/admin; webpack -p)
 	(cd client/api; webpack -p)
 	(cd client/editor; webpack -p)
 	(cd client/overlay; webpack -p)
+else
+	(cd client/admin; webpack -d --watch) &
+	(cd client/api; webpack -d --watch) &
+	(cd client/editor; webpack -d --watch) &
+	(cd client/overlay; webpack -d --watch) &
 fi
