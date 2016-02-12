203b837b5346886443a2946a6c140caa1c357e15
Tim Kendrick <timkendrick@gmail.com>
2016-02-12 23:10:30 +0000

    Use openresty for router

--- services/nginx/init.sh
+++ services/nginx/init.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-rm -rf /etc/nginx/conf.d
-mkdir -p /etc/nginx/conf.d
-cp -r /etc/nginx/template.d/* /etc/nginx/conf.d
-sed -i 's/\$HOST/'$HOST'/g' /etc/nginx/conf.d/*.conf
+rm -rf /opt/openresty/nginx/conf/conf.d
+mkdir -p /opt/openresty/nginx/conf/conf.d
+cp -r /opt/openresty/nginx/conf/template.d/* /opt/openresty/nginx/conf/conf.d
+sed -i 's/\$HOST/'$HOST'/g' /opt/openresty/nginx/conf/conf.d/*.conf
