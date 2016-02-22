291c032418c9bf5f17414cd69c58b391042be81f
Tim Kendrick <timkendrick@gmail.com>
2016-02-22 01:25:30 +0000

    Allow path in www subdomain Handlebars helper

--- workers/www/src/helpers/url.js
+++ workers/www/src/helpers/url.js
@@ -6,11 +6,15 @@ var Handlebars = require('handlebars');
 
 module.exports['subdomain'] = function(options) {
 	var host = options.hash.host;
+	var path = options.hash.path || '/';
+	var pathname = path.split('?')[0];
+	var search = path.split('?')[1] || '';
 	var subdomain = options.hash.subdomain || null;
 	var location = Handlebars.Utils.extend({}, host, {
 		hostname: (subdomain ? subdomain + '.' : '') + host.hostname,
 		port: (host.port === (host.protocol === 'https:' ? 443 : 80) ? null : host.port),
-		pathname: '/'
+		pathname: pathname,
+		search: search
 	});
 	return url.format(location);
 };
