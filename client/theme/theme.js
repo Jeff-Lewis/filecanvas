1f4213fa50da1024abc5a7d7676bbb0695376ecd
Tim Kendrick <timkendrick@gmail.com>
2016-03-02 15:33:53 +0000

    Move theme date localization and overlay description code into client themes library

--- client/theme/theme.js
+++ client/theme/theme.js
@@ -1,3 +1,44 @@
 'use strict';
 
 require('./lib/overlay');
+
+var bytes = require('bytes');
+
+(function($) {
+
+	$(function() {
+		initLocalizedDates();
+		initOverlayDescriptions();
+	});
+
+
+	function initLocalizedDates() {
+		$('time[datetime]').each(function(index, element) {
+			var $element = $(this);
+			var timestamp = Number($element.attr('datetime'));
+			var date = new Date(timestamp *	1000);
+			var dateString = getLocalizedDateString(date);
+			$element.text(dateString);
+		});
+	}
+
+	function initOverlayDescriptions() {
+		$('[data-file]').each(function(index, element) {
+			var $element = $(this);
+			var filename = $element.attr('data-file');
+			var timestamp = Number($element.attr('data-file-modified'));
+			var modifiedDate = new Date(timestamp *	1000);
+			var modifiedLabel = getLocalizedDateString(modifiedDate);
+			var filesizeLabel = bytes(Number($element.attr('data-file-size')), { decimalPlaces: 1 });
+			var description = filename + '\n' + modifiedLabel + ' – ' + filesizeLabel;
+			$element.attr('data-overlay-description', description);
+		});
+	}
+
+	function getLocalizedDateString(value) {
+		var DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+		var MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+		var date = new Date(value);
+		return DAYS[date.getDay()] + ' ' + date.getDate() + ' ' + MONTHS[date.getMonth()] + ' ' + date.getFullYear();
+	}
+})($);
