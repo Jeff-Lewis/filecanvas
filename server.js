83b94bbb9e1bfc5ed79bc46d3c06bc8aacc2baa6
Tim Kendrick <timkendrick@gmail.com>
2016-03-02 19:44:44 +0000

    Merge bootstrap script into main server script

--- server.js
+++ server.js
@@ -1,3 +1,67 @@
 'use strict';
 
-require('./src/bootstrap');
+var config = require('./src/config');
+
+if (config.newRelic) { require('newrelic'); }
+
+var DatabaseService = require('./src/services/DatabaseService');
+var CacheService = require('./src/services/CacheService');
+
+var routerApp = require('./src/apps/router');
+var serve = require('./src/serve');
+var captureErrors = require('./src/utils/captureErrors');
+
+var databaseService = new DatabaseService();
+var cacheService = new CacheService();
+
+Promise.all([
+	databaseService.connect(config.db.url),
+	cacheService.connect(config.cache.url)
+])
+	.then(function(connections) {
+		process.stdout.write('MongoDB database connected' + '\n');
+		process.stdout.write('Redis cache connected' + '\n');
+		return connections;
+	})
+	.then(function(connections) {
+		var database = connections[0];
+		var cache = connections[1];
+		return routerApp(database, cache, config);
+	})
+	.then(function(app) {
+		process.stdout.write('Express app initialized' + '\n');
+		return app;
+	})
+	.then(function(app) {
+		captureErrors({ external: true });
+		return app;
+	})
+	.then(function(app) {
+		var httpPort = config.http.port;
+		var httpTimeout = config.http.timeout;
+		var httpsPort = config.https.port;
+		var httpsTimeout = config.https.timeout;
+		var httpsOptions = config.https;
+
+		serve(app, {
+			httpPort: httpPort,
+			httpTimeout: httpTimeout,
+			httpsPort: httpsPort,
+			httpsTimeout: httpsTimeout,
+			httpsOptions: httpsOptions
+		});
+
+		return app;
+	})
+	.then(function(app) {
+		process.stdout.write('Server listening' +
+			' on HTTP port ' + app.get('httpPort') +
+			(app.get('httpsPort') ? ', HTTPS port ' + app.get('httpsPort') : '') +
+			'\n'
+		);
+	})
+	.catch(function(error) {
+		process.nextTick(function() {
+			throw error;
+		});
+	});
