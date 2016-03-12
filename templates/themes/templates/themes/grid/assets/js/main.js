b36f5024b7fe5606a68f82b2ba1c10238e675d7d
Tim Kendrick <timkendrick@gmail.com>
2016-03-12 00:29:36 +0000

    Disable links in Grid template editor preview

--- templates/themes/templates/themes/grid/assets/js/main.js
+++ templates/themes/templates/themes/grid/assets/js/main.js
@@ -3,29 +3,41 @@
 (function($){
 
 	$(function() {
-		initFilters();
+		initAdminMode();
+		initFilters('[data-toggle="filter"]');
 	});
 
+	function initAdminMode() {
+		var isWithinIframe = (window !== window.top);
+		if (isWithinIframe) {
+			preventNavigation();
+		}
 
-	function initFilters() {
-		var $toggleElements = $('[data-toggle="filter"]');
-		$toggleElements.each(function(index, element) {
+
+		function preventNavigation() {
+			$(document).on('click', 'a[href]', function(event) {
+				event.preventDefault();
+			});
+		}
+	}
+
+	function initFilters(selector) {
+		$(document).on('click', selector, function(event) {
+			event.preventDefault();
 			var $element = $(this);
-			$element.on('click', function(event) {
-				var $element = $(this);
-				var targetSelector = $element.attr('data-target');
-				var $targetElement = $(targetSelector);
-				var filter = $element.attr('data-filter');
-				$toggleElements.each(function(index, element) {
-					var $toggleElement = $(element);
-					var isActive = ($toggleElement.attr('data-filter') === filter);
-					$toggleElement.toggleClass('active', isActive);
-				});
-				$targetElement.children().each(function(index, element) {
-					var $itemElement = $(element);
-					var isVisible = (!filter || ($itemElement.attr('data-filter') === filter));
-					$itemElement.toggleClass('hidden', !isVisible);
-				});
+			var targetSelector = $element.attr('data-target');
+			var $targetElement = $(targetSelector);
+			var filter = $element.attr('data-filter');
+			var $toggleElements = $(selector);
+			$toggleElements.each(function(index, element) {
+				var $toggleElement = $(element);
+				var isActive = ($toggleElement.attr('data-filter') === filter);
+				$toggleElement.toggleClass('active', isActive);
+			});
+			$targetElement.children().each(function(index, element) {
+				var $itemElement = $(element);
+				var isVisible = (!filter || ($itemElement.attr('data-filter') === filter));
+				$itemElement.toggleClass('hidden', !isVisible);
 			});
 		});
 	}
