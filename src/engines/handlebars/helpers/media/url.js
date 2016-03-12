da5cae71be06c239a80d82e3da95f89378566b1e
Tim Kendrick <timkendrick@gmail.com>
2016-03-12 23:43:13 +0000

    Add handlebars URL helper

--- src/engines/handlebars/helpers/media/url.js
+++ src/engines/handlebars/helpers/media/url.js
@@ -1,6 +1,12 @@
 'use strict';
 
+var url = require('url');
 var slug = require('slug');
+
+module.exports['url'] = function(value, options) {
+	var hasProtocol = Boolean(url.parse(value).protocol);
+	return (hasProtocol ? value : 'http://' + value);
+};
 
 module.exports['slug'] = function(value, options) {
 	return slug(value, { lower: true });
