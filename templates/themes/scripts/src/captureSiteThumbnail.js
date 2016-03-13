4186baf2739c9c6e2445c0149001a26dc8bdd361
Tim Kendrick <timkendrick@gmail.com>
2016-03-13 19:17:42 +0000

    Strip date metadata from autogenerated PNG screenshots

--- templates/themes/scripts/src/captureSiteThumbnail.js
+++ templates/themes/scripts/src/captureSiteThumbnail.js
@@ -3,10 +3,13 @@
 var fs = require('fs');
 var http = require('http');
 var stream = require('stream');
+var util = require('util');
 var serveStatic = require('serve-static');
 var finalhandler = require('finalhandler');
 var screenshotStream = require('screenshot-stream');
 var imagemagick = require('imagemagick-native');
+var PngDatastream = require('png-datastream').default;
+var multipipe = require('multipipe');
 
 module.exports = function(options) {
 	options = options || {};
@@ -80,17 +83,91 @@ module.exports = function(options) {
 					reject(new Error(message));
 				})
 				.on('error', reject)
-				.pipe(resizeOptions ? imagemagick.streams.convert({
-					width: resizeOptions.width,
-					height: resizeOptions.width,
-					resizeStyle: 'aspectfit',
-					format: 'PNG',
-					quality: 100
-				}) : new stream.PassThrough())
+				.pipe(resizeOptions ? createPngStream({
+						width: resizeOptions.width,
+						height: resizeOptions.height
+					}) : new stream.PassThrough()
+				)
 				.on('error', reject)
 				.pipe(fs.createWriteStream(outputPath))
 				.on('error', reject)
 				.on('finish', resolve);
 		});
+	}
+
+	function createPngStream(options) {
+		var width = options.width;
+		var height = options.height;
+		var pngConverterStream = createPngConverterStream({
+			width: width,
+			height: height
+		});
+		var metadataStrippingStream = createPngTimeMetadataStripperStream();
+		return multipipe(pngConverterStream, metadataStrippingStream);
+
+
+		function createPngConverterStream(options) {
+			var width = options.width;
+			var height = options.height;
+			return imagemagick.streams.convert({
+				width: width,
+				height: height,
+				resizeStyle: 'aspectfit',
+				format: 'PNG',
+				quality: 100
+			});
+		}
+
+		function createPngTimeMetadataStripperStream() {
+
+			function PngTimeDateMetadataStripperStream() {
+				if (!(this instanceof PngTimeDateMetadataStripperStream)) {
+					return new PngTimeDateMetadataStripperStream();
+				}
+
+				this._chunks = [];
+
+				stream.Transform.call(this, options);
+			}
+
+			util.inherits(PngTimeDateMetadataStripperStream, stream.Transform);
+
+			PngTimeDateMetadataStripperStream.prototype._transform = function(chunk, enc, done) {
+				this._chunks.push(chunk);
+				done();
+			};
+
+			PngTimeDateMetadataStripperStream.prototype._flush = function(done) {
+				var inputBuffer = Buffer.concat(this._chunks);
+				this._chunks = null;
+				var png = deserializePngBuffer(inputBuffer);
+				png.setChunks(png.chunks.filter(function(chunk) {
+					return !isDateMetadataChunk(chunk);
+				}));
+				var outputBuffer = serializePngBuffer(png);
+				this.push(outputBuffer);
+				done();
+
+				function isDateMetadataChunk(chunk) {
+					if (chunk.type.type === 'tIME') { return true; }
+					if (chunk.type.type === 'tEXt') {
+						if (chunk.keyword === 'date:create') { return true; }
+						if (chunk.keyword === 'date:modify') { return true; }
+					}
+					return false;
+				}
+			};
+
+			return new PngTimeDateMetadataStripperStream();
+
+
+			function deserializePngBuffer(buffer) {
+				return PngDatastream.fromArrayBuffer(new Uint8Array(buffer).buffer);
+			}
+
+			function serializePngBuffer(png) {
+				return new Buffer(new Uint8Array(png.toArrayBuffer()));
+			}
+		}
 	}
 };
