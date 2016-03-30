6f4f1409b6388eae238cdf998fbcc47cd4786631
Tim Kendrick <timkendrick@gmail.com>
2016-03-30 15:33:04 +0100

    Add analytics for uncaught client-side exceptions

--- templates/app/client/analytics/analytics.js
+++ templates/app/client/analytics/analytics.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var StackFrame = require('stackframe');
+var StackTrace = require('stacktrace-js');
+
 require('./lib/analytics');
 
 var SegmentAnalyticsAdapter = require('./lib/adapters/SegmentAnalyticsAdapter');
@@ -11,6 +14,19 @@ $(function() {
 
 	$.fn.analytics.track = createTrackingFunction(function(trackingId, trackingData, callback) {
 		adapter.track(trackingId, trackingData, callback);
+	});
+
+	handleUncaughtExceptions(function(error, stackFrames) {
+		var errorFrame = stackFrames[0];
+		var trackingId = 'error:client';
+		var trackingData = {
+			message: error.toString(),
+			source: errorFrame.fileName,
+			line: errorFrame.lineNumber,
+			column: errorFrame.columnNumber,
+			stack: stackFrames.join('\n')
+		};
+		adapter.track(trackingId, trackingData);
 	});
 
 
@@ -57,6 +73,33 @@ $(function() {
 				trackEvent(trackingId, trackingData, function() {
 					element.submit();
 				});
+			}
+		};
+	}
+
+	function handleUncaughtExceptions(callback) {
+		window.onerror = function(message, source, line, column, error) {
+			if (error) {
+				StackTrace.fromError(error).then(function(stackFrames) { callback(error, stackFrames); }).catch(logError);
+			} else {
+				try {
+					error = new Error(message);
+					var stackFrames = [
+						new StackFrame({ fileName: source, lineNumber: line, columnNumber: column })
+					];
+					callback(error, stackFrames);
+				} catch (err) {
+					logError(err);
+				}
+			}
+
+			function logError(error) {
+				if (!window.console) { return; }
+				if (window.console.error) {
+					window.console.error(error);
+				} else {
+					window.console.log(error);
+				}
 			}
 		};
 	}
