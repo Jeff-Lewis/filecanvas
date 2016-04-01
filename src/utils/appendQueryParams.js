584cc49fbd5aaa5d2cf2bad155c9b5a0fdec18c5
Tim Kendrick <timkendrick@gmail.com>
2016-04-02 00:20:43 +0100

    Fix appendQueryParams helper when no params specified

--- src/utils/appendQueryParams.js
+++ src/utils/appendQueryParams.js
@@ -6,5 +6,5 @@ module.exports = function(url, params) {
 	if (!params) { return url; }
 	var queryString = serializeQueryParams(params);
 	var urlHasParams = (url.indexOf('?') !== -1);
-	return url + (urlHasParams ? '&' : '?') + queryString;
+	return url + (queryString ? (urlHasParams ? '&' : '?') + queryString : '');
 };
